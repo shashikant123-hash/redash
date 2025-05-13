e51fdf27357a64273ba74dd2b796dc3363555d8f
Shashikant <shashikant.zanje@mialotech.com>
2025-05-13 11:59:39 +0300

    Added Netlify Identity login

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect } from 'react';
+import netlifyIdentity from 'netlify-identity-widget';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  useEffect(() => {
+    netlifyIdentity.init(); // initialize the widget
+  }, []);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div className="App">
+      <h1>Welcome to Your Dashboard</h1>
+      <button onClick={() => netlifyIdentity.open()}>Log in / Sign up</button>
+    </div>
+  );
+}
+
+export default App;
